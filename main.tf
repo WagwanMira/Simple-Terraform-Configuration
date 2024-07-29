5977334890db176301026790417c1a60faa72bd7
Dmitry <dimaholoimov@yandex.ru>
2024-07-29 13:01:06 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,3 @@
-
 terraform {
   required_providers {
     yandex = {
@@ -7,14 +6,8 @@ terraform {
   }
   required_version = ">= 0.13"
 }
-
-// Terraform должен знать ключ, для выполнения команд по API
-
-// Определение переменной, которую нужно будет задать
 variable "yc_token" {}
-
 provider "yandex" {
   zone = "ru-central1-a"
   token = var.yc_token
-
 }
