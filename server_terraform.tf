aaffa509a20e15d18159b9a655c0cb5e7b932c0b
Dmitry <dimaholoimov@yandex.ru>
2024-07-29 13:02:45 +0300

    Update server_terraform.tf

--- server_terraform.tf
+++ server_terraform.tf
@@ -1,24 +1,3 @@
-
-
- 
-
-#terraform {
-#  required_providers {
-#    yandex = {
-#      source = "yandex-cloud/yandex"
-#    }
-#  }
-#  required_version = ">= 0.13"
-#}
-
-
-#variable "yc_token" {}
-
-#provider "yandex" {
-#  zone = "ru-central1-a"
-#  token = var.yc_token
-#}
-
 resource "yandex_compute_instance" "default" {
   name        = "test"
   platform_id = "standard-v1"
